df5226d77103c343c57abfe101ea9be7565740ca
Alex Chien <alex.chien@koocaa.com>
2017-10-16 15:49:40 +0800

    assets update

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -8,4 +8,4 @@ Rails.application.config.assets.version = '1.0'
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-Rails.application.config.assets.precompile += %w( home.js vendor.js vendor.css ckeditor/* )
+Rails.application.config.assets.precompile += %w( home.js vendor.js vendor.css ckeditor/* ie.js )
